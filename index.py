337b743ea43a3141bc43bbe03ee90bd2a2c6168e
Thomas Dybdahl Ahle <thomas@ahle.dk>
2015-07-31 13:54:00 +0200

    moved to jinja

---
+++ index.py
@@ -0,0 +1,35 @@
+from jinja2 import Environment, FileSystemLoader
+from collections import namedtuple
+
+Person = namedtuple('Person', ['name', 'website', 'photo'])
+Person.__new__.__defaults__ = ('Anonymous', '', '')
+Paper = namedtuple('Paper', ['title', 'authors', 'year', 'conference', 'comment', 'files'])
+Conference = namedtuple('Conference', ['name'])
+File = namedtuple('File', ['format', 'href'])
+
+class Vars:
+    coauthors = {
+        'thdy': Person('Thomas Dybdahl Ahle', '/', 'me.png'),
+        'pagh': Person('Rasmus Pagh', 'https://www.itu.dk/people/pagh/'),
+        'ilya': Person('Ilya Razenshteyn', 'http://www.ilyaraz.org/'),
+        'fran': Person('Francesco Silvestri', 'http://itu.dk/people/fras/')
+    }
+    me = coauthors['thdy']
+
+    conferences = {
+        'subm': Conference('Submitted'),
+        'soda': Conference('Proceedings of Symposium on Discrete Algorithms')
+    }
+
+    papers = [
+        Paper('On the Complexity of Maximum Inner Product Search',
+            ['thdy', 'pagh', 'ilya', 'fran'], 2015, 'subm', '',
+            files=[File('pdf','papers/SODA_2016_v1.pdf')])
+    ]
+
+    join_authors = lambda ps: ', '.join(Vars.coauthors[p].name for p in ps)
+
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('index.html')
+print(template.render(Vars.__dict__))
+
