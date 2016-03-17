be65094e3af131f44d27fb3710f9d20855145d78
Thomas Dybdahl Ahle <thomas@ahle.dk>
2016-03-17 16:27:39 +0100

    More about

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 python3 -m pip install --upgrade pip
 python3 index.py > output.html
 scp output.html ssh.itu.dk:~/public_html/index.html
+scp style.css ssh.itu.dk:~/public_html/style.css
 rm output.html
 git add .
 git commit
