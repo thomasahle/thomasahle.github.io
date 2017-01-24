36aa4ddc0a23fb6d04c27b1d8604fa24e37350f9
Thomas Dybdahl Ahle <thomas@ahle.dk>
2017-01-24 15:33:37 +0100

    Remember to add the script

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ python3 -m pip install --upgrade pip
 python3 index.py > output.html
 scp output.html ssh.itu.dk:~/public_html/index.html
 scp style.css ssh.itu.dk:~/public_html/style.css
+scp script.js ssh.itu.dk:~/public_html/script.js
 rm output.html
 git add .
 git commit
