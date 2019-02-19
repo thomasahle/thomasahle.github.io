85cafea4774a35563c925971d1a4f76547955227
Thomas Dybdahl Ahle <thomas@ahle.dk>
2019-02-19 12:54:17 +0100

    auto build

--- update.sh
+++ update.sh
@@ -5,17 +5,11 @@ git commit
 git push
 
 python3 -m pip install --upgrade pip
-mkdir compiled
 
-python3 render_html.py templates/index.html > compiled/index.html
-python3 render_tex.py templates/cv.tex > compiled/cv.tex
-
-cd compiled
-pdflatex cv.tex
-cd ..
+./build.sh
 
 scp compiled/index.html ssh.itu.dk:~/public_html/
-scp compiled/cv.pdf ssh.itu.dk:~/public_html/
+scp compiled/*.pdf ssh.itu.dk:~/public_html/
 scp -r static/ ssh.itu.dk:~/public_html/
 
 
