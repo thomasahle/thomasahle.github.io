46ee1adb20ea14243fdc2b10671453d4f84a3ba0
Thomas Dybdahl Ahle <thomas@ahle.dk>
2019-09-25 10:58:07 +0200

    Added learnit links

--- pcpp_data.py
+++ pcpp_data.py
@@ -28,13 +28,13 @@ class Vars:
             , Lecture(title = 'Streams'
                     , desc = 'Java 8 parallel streams for bulk data and parallel array prefix operations. Functional interfaces, lambda expressions, method reference expressions.'
                     , materials = 'Sestoft: Java Precisely <b>3rd edition</b> sections 11.13, 11.14, 23, 24, 25; get <a href="https://learnit.itu.dk/mod/resource/view.php?id=106234">draft on LearnIT</a> and <a href="https://www.itu.dk/people/sestoft/javaprecisely/javaprecisely3-examples.tgz">associated example code</a>. Also <a href="words.zip">File with English words</a>.'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106669'
                     , date = startdate + timedelta(weeks=3)
                     )
             , Lecture(title = 'Threads and Locks 2'
                     , desc = 'Tasks and the Java executor framework. Concurrent pipelines, wait() and notifyAll().'
                     , materials = 'Goetz chapters 5.3, 6, 8; Bloch items 68, 69'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106670'
                     , date = startdate + timedelta(weeks=4)
                     )
             , Break(title = 'Lecture Cancelled'
@@ -43,7 +43,7 @@ class Vars:
             , Lecture(title = 'Threads and Locks 3'
                     , desc = 'Concurrent hash maps: performance and scalability case study.'
                     , materials = 'Goetz chapter 11, 13.5'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106673'
                     , date = startdate + timedelta(weeks=6)
                     )
             #, Lecture(title = 'GUI applications'
@@ -58,37 +58,37 @@ class Vars:
             , Lecture(title = 'Threads and Locks 4'
                     , desc = 'Testing concurrent programs.'
                     , materials = 'Goetz chapter 12; Herlihy and Shavit chapter 3'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106675'
                     , date = startdate + timedelta(weeks=8)
                     )
             , Lecture(title = 'Transactional Memory'
                     , desc = 'Transactional memory with Multiverse: <a href="http://itu.dk/people/fbie/multiverse-javadoc/index.html?org/multiverse/api/package-summary.html">Multiverse API Javadoc</a> and file <a href="multiverse-core-0.7.0.jar">multiverse-core-0.7.0.jar</a>.'
                     , materials = 'Harris et al 2008 paper; Herlihy and Shavit sections 18.1-18.2; Cascaval et al 2008 paper; Eidenbenz, Wattenhofer 2011 paper;'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106676'
                     , date = startdate + timedelta(weeks=9)
                     )
             , Lecture(title = 'Title to be Determined'
                     , desc = 'Optimistic concurrency, lock-free data structures, Treiber stack, compare-and-swap. Consensus number.'
                     , materials = 'Goetz chapter 15; Herlihy and Shavit sections 5.1-5.2'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106677'
                     , date = startdate + timedelta(weeks=10)
                     )
             , Lecture(title = 'Title to be Determined'
                     , desc = 'The Michael and Scott queue, progress concepts, Union Find, work-stealing queues.'
                     , materials = 'Michael and Scott paper and Chase and Lev paper sections 1, 2 and 5'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106678'
                     , date = startdate + timedelta(weeks=11)
                     )
             , Lecture(title = 'Title to be Determined'
                     , desc = 'Introduction to message passing concurrency (no mutable shared memory), Introduction to Erlang, Introduction to Java+Akka.'
                     , materials = 'Erlang chapter 1, 2, 5'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106679'
                     , date = startdate + timedelta(weeks=12)
                     )
             , Lecture(title = 'Title to be Determined'
                     , desc = 'Message passing (no mutable shared memory), Erlang, Java+Akka.'
                     , materials = 'Just the slides'
-                    , learnit = '#'
+                    , learnit = 'https://learnit.itu.dk/mod/assign/view.php?id=106680'
                     , date = startdate + timedelta(weeks=13)
                     )
             , Lecture(title = 'Exam preparation'
