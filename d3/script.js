78e9ba93ff8f48c332da3461872c5241f794f406
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-05-02 01:34:16 +0200

    got arc working

--- d3/script.js
+++ d3/script.js
@@ -1,6 +1,48 @@
 // Can get some visual ideas from here
 // https://observablehq.com/@mbostock/tree-of-life
 //
+function link(curve) {
+  var radius = null,
+     angle = null;
+  function link(d) {
+     context = d3.path();
+     curve(context, radius(d.source), angle(d.source), radius(d.target), angle(d.target));
+     return context;
+  }
+  link.radius = function(f) {
+     radius = f;
+     return link;
+  };
+  link.angle = function(f) {
+     angle = f;
+     return link;
+  };
+  return link;
+}
+
+function curveArc(context, r0, a0, r1, a1) {
+  var p0 = d3.pointRadial(a0, r0),
+      p1 = d3.pointRadial(a0, (r0 + r1) / 2),
+      p2 = d3.pointRadial(a1, (r0 + r1) / 2),
+      p3 = d3.pointRadial(a1, r1);
+   /*if (r0 == r1 && a0 == a1) {
+      console.log([r0, a0, r1, a1]);
+      return;
+   }*/
+   //console.log([a0, a1, (a1-a0), (a1-a0+2*Math.PI)%(2*Math.PI)]);
+  context.moveTo(p0[0], p0[1]);
+  context.lineTo(p1[0], p1[1]);
+   //if (Math.abs(a0 - a1) > 1e-3)
+      // We can make a simple a test, since the tree can never wrap all the way around
+     context.arc(0, 0, (r0+r1)/2, a0-Math.PI/2, a1-Math.PI/2, a1<a0);
+  context.moveTo(p2[0], p2[1]);
+  context.lineTo(p3[0], p3[1]);
+}
+
+function linkRadial2() {
+   return link(curveArc);
+}
+
 
 class RadialTree {
    constructor() {
@@ -59,6 +101,15 @@ class RadialTree {
          }
       });
    }
+   isAlive() {
+      const level = this.root.height;
+      let res = false;
+      this.root.each(node => {
+         if (node.depth == level && node.data.alive)
+            res = true;
+      });
+      return res;
+   }
    mount(svg) {
       this.svg = svg;
       this.gLink = svg.append("g")
@@ -72,9 +123,7 @@ class RadialTree {
    }
    update() {
       this.tree(this.root);
-      const nodes = this.root.descendants();
       const links = this.root.links();
-      const node = this.gNode.selectAll('g').data(nodes, d => d.data.id);
 
       // Compute the new tree layout.
       //const size = size_radial();
@@ -90,54 +139,41 @@ class RadialTree {
       // Some sort of hack to make the window realize it needs to resize?
          .tween("resize", window.ResizeObserver ? null : () => () => this.svg.dispatch("toggle"));
 
-      const nodeEnter = this.svg.enter().append('g')
-         .attr("transform", d => {
-            if (d.parent)
-               return `translate(${d.parent.x0},${d.parent.y0})`;
-            return `translate(0,0)`;
-         })
-         .attr("fill-opacity", 0)
-         .attr("stroke-opacity", 0);
 
-      // Transition nodes to their new position.
-      // Can I just do nodeEnter.merge(node)? Er det grimt?
-      node.merge(nodeEnter)
-      // Inherit timing from main transition
-         .transition(transition)
-         .attr("transform", d => `
-              rotate(${d.x * 180 / Math.PI - 90})
-              translate(${d.y},0)
-            `)
-         //.attr("transform", d => `translate(${d.x},${d.y})`)
-         .attr("fill-opacity", 1)
-         .attr("stroke-opacity", 1);
-
-
-      // Update the links…
+      // Update the links. They take the id of their target.
       const link = this.gLink.selectAll("path")
          .data(links, d => d.target.data.id);
 
       // Helper function for beizer paths
       // const diagonal = d3.linkVertical().x(d => d.x).y(d => d.y);
-      const diagonal = d3.linkRadial().angle(d => d.x).radius(d => d.y);
+      const diagonal = linkRadial2().angle(d => d.x).radius(d => d.y);
 
       // Enter any new links at the parent's previous position.
-      const linkEnter = link.enter().append("path")
-         .attr("d", d => {
-            // Start with a path that just goes from a point to itself.
-            // This is a dummy link object, since it has a source and a target.
-            const o = {x: d.source.x0, y: d.source.y0};
-            return diagonal({source: o, target: o});
-         });
+      const linkEnter = link.enter().append("path");
 
       // Transition links to their new position.
       // Note: each link is an object that defines source and target properties
       link.merge(linkEnter).transition(transition)
-         .attr("d", diagonal);
+         // We need to storre the old positions before they get updated at the end
+         // of the function. It turns out attrTween is too late.
+         .attr('d', li => {
+            li.source.x00 = li.source.x0;
+            li.source.y00 = li.source.y0;
+            // If there is no x0 (the newly entered items)i
+            // we make it pop out of the parent.
+            li.target.x00 = li.target.x0 || li.source.x0;
+            li.target.y00 = li.target.y0 || li.source.y0;
+         })
+         .attrTween("d", d => t => diagonal(
+               {source: {x: (1-t)*d.source.x00 + t*d.source.x,
+                         y: (1-t)*d.source.y00 + t*d.source.y},
+                target: {x: (1-t)*d.target.x00 + t*d.target.x,
+                         y: (1-t)*d.target.y00 + t*d.target.y}})
+         );
+         //.attr("d", diagonal);
 
       // We just use those for reset.
       // But actually, the new structure doesn't require resets...
-      node.exit().remove();
       link.exit().remove();
 
       // Stash the old positions for transition.
