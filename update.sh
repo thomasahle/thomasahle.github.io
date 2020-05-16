cae48fe8c7538125ecf5b8272d1f3f6fa21fe4f9
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-05-16 22:09:07 +0200

    new update script

--- update.sh
+++ update.sh
@@ -5,8 +5,19 @@ git commit
 git push
 
 python3 -m pip install --upgrade pip
+python3 -m pip install jinja2
 
 ./build.sh
+
+git checkout master
+git checkout gh-pages -- compiled
+git checkout gh-pages -- static
+mv compiled/* .
+rmdir compiled
+git add .
+git commit -m "update"
+git push
+git checkout gh-pages
 
 #scp compiled/index.html ssh.itu.dk:~/public_html/
 #scp compiled/pcpp.html ssh.itu.dk:~/public_html/teaching/pcpp2019/index.html
