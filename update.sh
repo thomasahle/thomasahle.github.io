daff51a9e6d0c7d00dc0ea6dd8de1857e1c5eb07
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-05-16 22:23:11 +0200

    test

--- update.sh
+++ update.sh
@@ -13,8 +13,9 @@ git checkout master
 # compiled will be in working space, not repo
 #git checkout gh-pages -- compiled
 git checkout gh-pages -- static
-mv compiled/* .
-rmdir compiled
+mv compiled/*.html .
+mv compiled/*.pdf .
+rm -rf compiled
 git add .
 git commit -m "update"
 git push
