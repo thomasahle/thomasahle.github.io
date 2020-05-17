733acbe8963d47c5c7b7f7f0e90e4d4b05932834
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-05-17 12:42:30 +0200

    dist

---
+++ blog/plot.js
@@ -0,0 +1,110 @@
+class JoyPlot {
+   constructor(graph) {
+      this.graph = graph;
+      this.margin = {top: 10, right: 15, bottom: 50, left: 15};
+      this.width = 800;
+   }
+   mount(svg) {
+      this.svg = svg;
+      svg.attr('width', this.width);
+      this.gxAxis = svg.append("g");
+      this.gyAxis = svg.append("g");
+      svg.append('g').attr('id', 'rows');
+      graph.dispatch.on('change', () => this.update());
+   }
+   update() {
+      const series = [];
+      this.graph.root.each(d => {
+         while (series.length <= d.depth)
+            series.push([]);
+         while (series[d.depth].length <= d.data.value)
+            series[d.depth].push(0);
+         series[d.depth][d.data.value]++;
+      });
+      // Add extra row of zeros
+      for (let row of series) {
+         row.push(0);
+         row.push(0);
+      }
+      series.shift();
+
+      const transition = this.svg.transition()
+         .duration(this.graph.duration);
+
+      const margin = this.margin;
+      const width = this.width;
+      const step = 17;
+      const overlap = 8;
+      const max = d3.max(series, d => d3.max(d));
+      const maxz = d3.max(series, (d,i) => overlap * step * d3.max(d)/max - i * step);
+      // The real step formula is step = (stop - start) / Math.max(1, n - paddingInner + paddingOuter * 2)
+      const len = series.length;
+      const pad = 1;
+      const margin_top = margin.top + len/Math.max(1,len-pad) * maxz;
+      const height = series.length * step + margin_top + margin.bottom;
+      this.svg.transition(transition).attr('height', height);
+      const x = d3.scalePoint()
+          .domain(d3.range(0, d3.max(series, d => d.length)))
+          .range([margin.left, width - margin.right]);
+      const y = d3.scalePoint()
+          .domain(d3.range(1, series.length+1))
+          .range([margin_top, height - margin.bottom]);
+      const z = d3.scaleLinear()
+          .domain([0, max]).nice()
+          .range([0, -overlap * y.step()]);
+      this.gxAxis
+         .transition(transition)
+         .call(g => g
+           .attr("transform", `translate(0,${height - margin.bottom})`)
+           .call(d3.axisBottom(x)
+               .ticks(width / 80)
+               .tickSizeOuter(0)));
+      this.gyAxis
+         .transition(transition)
+         .call(g => g
+             .attr("transform", `translate(${margin.left},0)`)
+             .call(d3.axisLeft(y).tickSize(0).tickPadding(4))
+             .call(g => g.select(".domain").remove()));
+      const area = d3.area()
+          .curve(d3.curveMonotoneX)
+          .x((d, i) => x(i))
+          .y0(0)
+          .y1(d => z(d));
+      const area_flat = d3.area()
+          .curve(d3.curveBasis)
+          .x((d, i) => x(i))
+          .y0(0)
+          .y1(0);
+      let line = area.lineY1();
+      let line_flat = area_flat.lineY1();
+
+      const rows = this.svg.select('#rows')
+         .selectAll('g')
+         .data(series, (d,i) => i)
+         .join(enter => {
+            let g = enter.append('g')
+               .attr("transform", (d,i) => `translate(0,${i > 0 ? y(i) : y(i+1)})`);
+            g.append('path')
+               .attr('class', 'area')
+               .attr("d", d => area_flat(d));
+            g.append('path')
+               .attr('class', 'line')
+               .attr("d", d => line_flat(d))
+               .attr("stroke-opacity", "0") ;
+            return g;
+         });
+      rows
+         .transition(transition)
+         .attr("transform", (d,i) => `translate(0,${y(i+1)})`);
+      rows
+         .select('path.area')
+         .transition(transition)
+         .attr("d", d => area(d));
+      rows
+         .select('path.line')
+         .transition(transition)
+         .attr("d", d => line(d))
+         .attr("stroke-opacity", "1");
+
+   }
+}
---
+++ blog/tree.js
@@ -0,0 +1,211 @@
+// Can get some visual ideas from here
+// https://observablehq.com/@mbostock/tree-of-life
+//
+function link(curve) {
+  var radius = null,
+     angle = null;
+  function link(d) {
+     context = d3.path();
+     curve(context, radius(d.source), angle(d.source), radius(d.target), angle(d.target));
+     return context;
+  }
+  link.radius = function(f) {
+     radius = f;
+     return link;
+  };
+  link.angle = function(f) {
+     angle = f;
+     return link;
+  };
+  return link;
+}
+
+function curveArc(context, r0, a0, r1, a1) {
+  var p0 = d3.pointRadial(a0+Math.PI/2, r0),
+      p1 = d3.pointRadial(a0+Math.PI/2, (r0 + r1) / 2),
+      p2 = d3.pointRadial(a1+Math.PI/2, (r0 + r1) / 2),
+      p3 = d3.pointRadial(a1+Math.PI/2, r1);
+
+  context.moveTo(p0[0], p0[1]);
+  context.lineTo(p1[0], p1[1]);
+  context.arc(0, 0, (r0+r1)/2, a0, a1, a1<a0);
+  context.lineTo(p3[0], p3[1]);
+
+  // context.moveTo(p0[0], p0[1]);
+  // context.arc(0, 0, r0, a0-Math.PI/2, a1-Math.PI/2, a1<a0);
+  // context.lineTo(p3[0], p3[1]);
+}
+
+function linkRadial2() {
+   return link(curveArc);
+}
+
+class RadialTree {
+   constructor(radius) {
+      this.radius = radius;
+      this.tree = d3.tree().size([2*Math.PI, this.radius]);
+      this.duration = 1000;
+      this.dispatch = d3.dispatch('change');
+   }
+   reset(init_degree, p, t, d) {
+      this.p = p;
+      this.t = t;
+      this.d = d;
+      this.root = d3.hierarchy({
+         name: '',
+         value: 0,
+         id: 0,
+         alive: true
+      });
+      this.id_counter = 1;
+      this.root.x0 = 0;
+      this.root.y0 = 0;
+      for (let i = 0; i < init_degree; i++) {
+         this.insert(this.root, {name: '', value: 0, alive: true});
+      }
+      this.total_size = 1;
+   }
+   insert(par, data) {
+      data.id = this.id_counter++;
+      const newNode = d3.hierarchy(data);
+      newNode.x0 = par.x0;
+      newNode.y0 = par.y0;
+      newNode.depth = par.depth + 1;
+      newNode.parent = par;
+      // Walk up the tree, updating the heights of ancestors as needed.
+      // Could perhaps be done also with newNode.decendants().each.
+      for(let height = 1, anc = par; anc != null; height++, anc=anc.parent) {
+         anc.height = Math.max(anc.height, height);
+      }
+      if (!par.children)
+         par.children = [];
+      par.children.push(newNode);
+   }
+   step() {
+      const level = this.root.height;
+      const level_size = Math.ceil(Math.pow(this.d, level)/this.total_size);
+      this.total_size *= level_size;
+      this.root.each(node => {
+         if (node.depth == level && node.data.alive) {
+            for (let i = 0; i < level_size; i++) {
+               const val = node.data.value + (Math.random() < this.p ? 1 : 0);
+               // const rat = this.t == 0 || this.t == 1 ? 0
+               //             : Math.log(this.t/this.p * (1-this.p)/(1-this.t));
+               // const sig = Math.pow(rat**2 * this.t*(1-this.t)*level, 1/3);
+               // Maybe the reason 1/3 is good is that I would actually prefer the
+               // process to die at some point, before the browswer gets overloaded.
+               const sig = Math.pow(this.t*(1-this.t)*level, 1/3);
+               this.insert(node, {
+                  name: val+'/'+level,
+                  value: val,
+                  alive: this.t >= this.p ? val >= this.t*level-sig : val <= this.t*level+sig,
+                  id: this.id_counter++
+               });
+            }
+         }
+      });
+   }
+   isAlive() {
+      const level = this.root.height;
+      let res = false;
+      this.root.each(node => {
+         if (node.depth == level && node.data.alive)
+            res = true;
+      });
+      return res;
+   }
+   mount(svg) {
+      this.svg = svg
+         .classed('radial_tree', true);
+      const r = this.radius;
+      const p = 15;
+      svg.attr('width', 2*r)
+         .attr('height', 2*r)
+         .attr('viewBox', [-r-p, -r-p, 2*r+2*p, 2*r+2*p]);
+      this.g = svg.append("g")
+   }
+   update() {
+      this.tree(this.root);
+      this.dispatch.call('change');
+      const links = this.root.links();
+      const nodes = this.root.descendants();
+
+      // Compute the new tree layout.
+      //const size = size_radial();
+      const transition = this.svg.transition()
+         .duration(this.duration)
+      // Animate resize of svg element to fit new data
+         //.attr("viewBox", [-margin.left, - margin.top, size.width, size.height])
+      // Some sort of hack to make the window realize it needs to resize?
+         .tween("resize", window.ResizeObserver ? null : () => () => this.svg.dispatch("toggle"));
+
+      // Update the links. They take the id of their target.
+      const link = this.g.selectAll("path")
+         .data(links, d => d.target.data.id);
+
+      // Helper function for beizer paths
+      // const diagonal = d3.linkVertical().x(d => d.x).y(d => d.y);
+      const diagonal = linkRadial2().angle(d => d.x).radius(d => d.y);
+
+      // Enter any new links at the parent's previous position.
+      const linkEnter = link.enter().append("path");
+
+      // Transition links to their new position.
+      // Note: each link is an object that defines source and target properties
+      link.merge(linkEnter)
+         // We hide root links, since they don't really exist
+         .attr('visibility', d => d.source.depth == 0 ? 'hidden' : 'visible')
+         .transition(transition)
+         // We need to storre the old positions before they get updated at the end
+         // of the function. It turns out attrTween is too late.
+         .attr('d', li => {
+            li.source.x00 = li.source.x0;
+            li.source.y00 = li.source.y0;
+            // If there is no x0 (the newly entered items)i
+            // we make it pop out of the parent.
+            li.target.x00 = li.target.x0 || li.source.x0;
+            li.target.y00 = li.target.y0 || li.source.y0;
+         })
+         .attrTween("d", d => t => diagonal(
+               {source: {x: (1-t)*d.source.x00 + t*d.source.x,
+                         y: (1-t)*d.source.y00 + t*d.source.y},
+                target: {x: (1-t)*d.target.x00 + t*d.target.x,
+                         y: (1-t)*d.target.y00 + t*d.target.y}})
+         );
+
+      // We do the nodes after the links to have them on top
+      const nodeUpdate = this.g.selectAll('g.node')
+         .data(nodes, d => d.data.id);
+
+      const nodeEnter = nodeUpdate.enter().append("g").classed('node', true)
+         .attr('transform', d => `translate(
+               ${d.parent ? d.parent.y0 * Math.cos(d.parent.x0) : 0},
+               ${d.parent ? d.parent.y0 * Math.sin(d.parent.x0) : 0})`);
+
+      // Make shadows
+      nodeEnter.append('circle')
+         .clone()
+         .classed('shadow', true)
+         .lower();
+
+      nodeUpdate.merge(nodeEnter)
+         .classed("inner", d => d.height > 0)
+         .classed("alive", d => d.data.alive)
+         .transition(transition)
+         .attr('transform', d => `translate(
+               ${d.y * Math.cos(d.x)},
+               ${d.y * Math.sin(d.x)})`);
+
+      // We just use those for reset.
+      // But actually, the new structure doesn't require resets...
+      link.exit().remove();
+      nodeUpdate.exit().remove();
+
+      // Stash the old positions for transition.
+      this.root.each(d => {
+         d.x0 = d.x;
+         d.y0 = d.y;
+      });
+   }
+}
+
