de51da9841e0b0cc0a54b6961b5602003d2a06e3
Thomas Dybdahl Ahle <thomas@ahle.dk>
2020-06-02 15:07:37 +0200

    Deploying

--- static/script.js
+++ static/script.js
@@ -1,18 +1,18 @@
 window.onload = function () {
    for (let div of document.querySelectorAll('.toggle-item')) {
       div.onclick = function() {
-          dataLayer.push({'event': 'toggle_'+div.id});
-          this
-            .querySelector('.abstract')
-            .classList.toggle('open');
+          gtag('event', 'toggle', {'value': div.id});
+         // Other options: event_category, event_label
+          this.querySelector('.abstract').classList.toggle('open');
       }
    }
    let img = document.getElementById('me');
    img.onmouseover = function() {
      img.setAttribute('src', 'static/water.jpg');
-     dataLayer.push({'event': 'hover_me'});
+     gtag('event', 'hover_me', {'value': true});
    }
    img.onmouseout = function() {
      img.setAttribute('src', 'static/potrait.jpg');
+     gtag('event', 'hover_me', {'value': false});
    }
 }
